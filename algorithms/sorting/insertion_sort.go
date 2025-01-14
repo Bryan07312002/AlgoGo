6f6c705e2486a49e7415a8e48275036100b3bab6
Bryan07312002 <contato.bryanbecker@gmail.com>
2025-01-14 15:46:06 -0300

    fix: insertion sort working

--- algorithms/sorting/insertion_sort.go
+++ algorithms/sorting/insertion_sort.go
@@ -1,24 +1,17 @@
 package sorting
 
-import "fmt"
-
+// time: O(n^2)
 func InsertionSort(n []int) []int {
-	fmt.Println(IntSliceToString(n))
-
-	fmt.Println("-----")
 	for i := 1; i < len(n); i++ {
-		pointer := i - 1
-
-		for pointer != 0 && n[pointer] > n[i] {
-			fmt.Println(pointer)
-			pointer -= 1
+		if n[i-1] > n[i] {
+			for j := i; j >= 1; j-- {
+				if n[j] < n[j-1] {
+					tmp := n[j]
+					n[j] = n[j-1]
+					n[j-1] = tmp
+				}
+			}
 		}
-
-		tmp := n[i]
-		n[i] = n[pointer]
-		n[pointer] = tmp
-
-		fmt.Println(IntSliceToString(n))
 	}
 
 	return n
--- algorithms/sorting/insertion_sort_test.go
+++ algorithms/sorting/insertion_sort_test.go
@@ -9,8 +9,27 @@ func TestSuiteInsertionSort(t *testing.T) {
 		got := InsertionSort(input)
 		for i := 0; i < len(expected); i++ {
 			if got[i] != expected[i] {
-				t.Errorf("expected: " + IntSliceToString(expected) +
-					" got: " + IntSliceToString(got))
+				t.Errorf(
+					"expected: %s got: %s",
+					IntSliceToString(expected),
+					IntSliceToString(got),
+				)
+				break
+			}
+		}
+	})
+
+	t.Run("should sort with repeted numbers", func(t *testing.T) {
+		input := []int{5, 4, 5, 3, 2, 1}
+		expected := []int{1, 2, 3, 4, 5, 5}
+		got := InsertionSort(input)
+		for i := 0; i < len(expected); i++ {
+			if got[i] != expected[i] {
+				t.Errorf(
+					"expected: %s got: %s",
+					IntSliceToString(expected),
+					IntSliceToString(got),
+				)
 				break
 			}
 		}
